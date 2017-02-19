158eb53a47d995b6ce425e7c2f683567e32eaa88
mahantesh hiremath <mahantesh hiremath>
2017-02-18 17:10:56 -0800

    review submission

--- script/tyb.js
+++ script/tyb.js
@@ -1,83 +1,84 @@
 jQuery(document).ready(function ($) {
-
-    $('#checkbox').change(function(){
-      setInterval(function () {
-        moveRight();
-      }, 3000);
-    });
-
-    var slideCount = $('#slider ul li').length;
-    var slideWidth = $('#slider ul li').width();
-    var slideHeight = $('#slider ul li').height();
-    var sliderUlWidth = slideCount * slideWidth;
-
-    $('#slider').css({ width: slideWidth, height: slideHeight });
-
-    $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-      $('#slider ul').animate({
-        left: + slideWidth
-      }, 300, function () {
-        $('#slider ul li:last-child').prependTo('#slider ul');
-        $('#slider ul').css('left', '');
-      });
-    };
-
-    function moveRight() {
-      $('#slider ul').animate({
-        left: - slideWidth
-      }, 300, function () {
-        $('#slider ul li:first-child').appendTo('#slider ul');
-        $('#slider ul').css('left', '');
-      });
-    };
-
-    $('a.control_prev').click(function () {
-      moveLeft();
-    });
-
-    $('a.control_next').click(function () {
+  $( "#slider" ).click(function() {
+    $('.pad').hide(400);
+  });
+  $('#checkbox').change(function(){
+    setInterval(function () {
       moveRight();
-    });
-
-  });    
-
-  $(".caption").imageTitle({
-    type:"caption"
+    }, 3000);
   });
 
-  var x = 0;
-  function myFunction() {
+  var slideCount = $('#slider ul li').length;
+  var slideWidth = $('#slider ul li').width();
+  var slideHeight = $('#slider ul li').height();
+  var sliderUlWidth = slideCount * slideWidth;
 
-  }
-  function init() {
-    window.addEventListener('scroll', function(e){
-      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 130,
-      header = document.querySelector("header");
-      if (distanceY >= shrinkOn) {
-       $("#mainLogo").hide(350);
-       $("#smallLogo").show("slow");
-       $('.pad').hide(400);
-     } else {
-      if ($("#mainLogo").hide()) {
-       $("#smallLogo").hide();
-       $("#mainLogo").show();
-     }
+  $('#slider').css({ width: slideWidth, height: slideHeight });
+
+  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+  $('#slider ul li:last-child').prependTo('#slider ul');
+
+  function moveLeft() {
+    $('#slider ul').animate({
+      left: + slideWidth
+    }, 400, function () {
+      $('#slider ul li:last-child').prependTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  function moveRight() {
+    $('#slider ul').animate({
+      left: - slideWidth
+    }, 400, function () {
+      $('#slider ul li:first-child').appendTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  $('a.control_prev').click(function () {
+    moveLeft();
+  });
+
+  $('a.control_next').click(function () {
+    moveRight();
+  });
+
+});    
+
+$(".caption").imageTitle({
+  type:"caption"
+});
+
+var x = 0;
+function myFunction() {
+
+}
+function init() {
+  window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    shrinkOn = 113,
+    header = document.querySelector("header");
+    if ((distanceY > shrinkOn)) {
+     $("#mainLogo").hide(450);
+     
+   } else {
+     $("#mainLogo").show(450);
+   }
+   if ((distanceY > 1)) {
+    $('.pad').hide(400);
    }
  });
-  }
-  window.onload = init();
+}
+window.onload = init();
 
-    $('.pad').hide();
-    $('.heading').click(function () {
-      $(this).next('.pad').slideToggle();
-      if($(this).find('.span1').attr('id') == 'yes') {
-        $(this).find('.span1').attr('id', '').html('&#8744;');
-      } else {
-        $(this).find('.span1').attr('id', 'yes').html('&#8743;');
-      }
-    });
+$('.pad').hide();
+$('.heading').click(function () {
+  $(this).next('.pad').slideToggle();
+  if($(this).find('.span1').attr('id') == 'yes') {
+    $(this).find('.span1').attr('id', '').html('&#8744;');
+  } else {
+    $(this).find('.span1').attr('id', 'yes').html('&#8743;');
+  }
+});
